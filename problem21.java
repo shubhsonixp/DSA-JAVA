af6e94137bfe3e734dd01e201aebd1ad3a70eef8
shubham verma <shubhsonixp@gmail.com>
2022-05-31 23:08:12 +0530

    Move all negative numbers to beginning and positive to end

---
+++ problem21.java
@@ -0,0 +1,15 @@
+//Move all negative numbers to beginning and positive to end
+import java.util.*;
+public class problem21 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sc.close();
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
